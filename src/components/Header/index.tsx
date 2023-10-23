3293ad783cf53a7029cee49d2738a1910cd8c019
Hillel1408 <etemax@bk.ru>
2023-10-23 17:21:02 +0300

    feat: верстка

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,8 +4,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@src/components";
 import { ROUTES } from "@src/constants/";
+import { useState } from "react";
+import classNames from "classnames";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  showMenu
+    ? (document.body.style.overflow = "hidden")
+    : (document.body.style.overflow = "initial");
+
   const navItems = [
     { text: "Product", href: "#" },
     { text: "Pricing", href: "#" },
@@ -13,28 +21,55 @@ const Header = () => {
 
   return (
     <header>
-      <div className="container py-[37px] flex items-center md:flex-col md:gap-3">
+      <div className="container py-[37px] flex items-center justify-between">
         <Link href={ROUTES.HOME}>
           <div className="w-[230px] h-[46px] relative">
             <Image src="/images/logo.svg" alt="" layout="fill" />
           </div>
         </Link>
 
-        <ul className="text-[#000] text-[20px] font-bold flex gap-7 ml-[55px] flex-auto md:ml-0">
-          {navItems.map((item, index) => (
-            <li key={index}>
-              <Link href="#">{item.text}</Link>
-            </li>
-          ))}
-        </ul>
+        <div
+          className={classNames(
+            "flex items-center flex-auto md:absolute md:left-0 md:right-0 bg-white md:flex-col md:top-0 md:bottom-0 md:z-10 md:py-10 md:gap-7 duration-200",
+            showMenu ? "md:opacity-100 md:visible" : "md:opacity-0 md:invisible"
+          )}
+        >
+          <ul className="text-[#000] text-[20px] font-bold flex gap-7 ml-[55px] flex-auto md:ml-0 md:flex-col md:flex-none">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <Link href="#">{item.text}</Link>
+              </li>
+            ))}
+          </ul>
 
-        <div className="flex gap-[35px] items-center">
-          <Link href="#" className="text-[#000] text-[20px] font-bold">
-            Sign In
-          </Link>
+          <div className="flex gap-[35px] items-center md:flex-col">
+            <Link href="#" className="text-[#000] text-[20px] font-bold">
+              Sign In
+            </Link>
 
-          <Button text="Join Waitlist" />
+            <Button text="Join Waitlist" />
+          </div>
+
+          <button
+            className="hidden md:block md:absolute top-3 right-3"
+            onClick={() => {
+              setShowMenu(false);
+            }}
+          >
+            X
+          </button>
         </div>
+
+        <button
+          className="hidden md:block"
+          onClick={() => {
+            setShowMenu(true);
+          }}
+        >
+          <svg className="h-[32px] w-[32px]" aria-hidden="true">
+            <use xlinkHref="/sprites/sprite.svg#menu"></use>
+          </svg>
+        </button>
       </div>
     </header>
   );
